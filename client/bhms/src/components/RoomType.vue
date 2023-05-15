fee3f36c19c20a338f42fc359f9cc695f5564cdb
bianxa56 <71631842+bianxa56@users.noreply.github.com>
2023-05-15 21:04:44 +0800

    added images to public, display room images using room names from database

--- client/bhms/src/components/RoomType.vue
+++ client/bhms/src/components/RoomType.vue
@@ -1,6 +1,6 @@
 <template>
     <q-card class="my-card" flat bordered style="margin: 15px;">
-        <q-img src="~assets/chateau/room-1.jpg">
+        <q-img :src="getRoomImage(room.name)">
         </q-img>
         <q-card-section>
             <div class="row no-wrap items-center">
@@ -36,9 +36,29 @@ export default {
         hover: false
       }
     },
+    methods: {
+      getRoomImage(roomName) {
+        let img = ""
+        console.log(roomName)
+        switch(roomName) {
+          case "Room 1": return 'room-1.jpg'
+          case "Room 2": return 'room-2.jpg'
+          case "Room 5": return 'room-5.jpg'
+          case "Room 6": return 'room-6.jpg'
+          case "Room 8": return 'room-8.jpg'
+          case "Room 9": return 'room-9.jpg'
+          case "Room 11": return 'room-11.jpg'
+          case "Room 12": return 'room-12.jpg'
+          case "Room 14": return 'room-14.jpg'
+          case "Room 15": return 'room-15.jpg'
+          case "Room 17": return 'room-17.jpg'
+        }
+      }
+    },
     props: ['room']
 }
 </script>
+
 <style scoped lang="sass">
 .my-card
   width: 100%
